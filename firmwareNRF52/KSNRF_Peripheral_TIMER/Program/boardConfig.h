0b03372823fd84357c2d0834fd1c93fc61dbc01a
Hom-Wang <wenhom.wang@gmail.com>
2017-12-02 11:24:43 +0800

    Add KSNRF_Peripheral_TIMER 12/02

---
+++ firmwareNRF52/KSNRF_Peripheral_TIMER/Program/boardConfig.h
@@ -0,0 +1,90 @@
+/**
+ *      __            ____
+ *     / /__ _  __   / __/                      __  
+ *    / //_/(_)/ /_ / /  ___   ____ ___  __ __ / /_ 
+ *   / ,<  / // __/_\ \ / _ \ / __// _ \/ // // __/ 
+ *  /_/|_|/_/ \__//___// .__//_/   \___/\_,_/ \__/  
+ *                    /_/   github.com/KitSprout    
+ * 
+ *  @file    boardConfig.h
+ *  @author  KitSprout
+ *  @date    01-Dec-2017
+ *  @brief   
+ * 
+ */
+
+/* Define to prevent recursive inclusion ---------------------------------------------------*/
+#ifndef __BOARDCONFIG_H
+#define __BOARDCONFIG_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes --------------------------------------------------------------------------------*/
+/* Exported types --------------------------------------------------------------------------*/
+/* Exported constants ----------------------------------------------------------------------*/
+
+#define KS_HW_BOARD_NAME              "KSDK"
+#define KS_HW_MCU_NAME                "NRF52832"
+
+#define KS_HW_USE_CLOCK_SOUCE_HSI     (0U)
+#define KS_HW_USE_CLOCK_SOUCE_HSE     (1U)
+#define KS_HW_CLOCK_SOUCE             KS_HW_USE_CLOCK_SOUCE_HSE
+
+
+/* -------- LED and KEY */
+
+#define LED_R_PIN                     6
+#define LED_R_PORT                    NRF_P0
+#define LED_R_Set()                   __GPIO_SET(LED_R_PORT, LED_R_PIN)
+#define LED_R_Reset()                 __GPIO_RST(LED_R_PORT, LED_R_PIN)
+#define LED_R_Toggle()                __GPIO_TOG(LED_R_PORT, LED_R_PIN)
+
+#define LED_G_PIN                     7
+#define LED_G_PORT                    NRF_P0
+#define LED_G_Set()                   __GPIO_SET(LED_G_PORT, LED_G_PIN)
+#define LED_G_Reset()                 __GPIO_RST(LED_G_PORT, LED_G_PIN)
+#define LED_G_Toggle()                __GPIO_TOG(LED_G_PORT, LED_G_PIN)
+
+#define LED_B_PIN                     8
+#define LED_B_PORT                    NRF_P0
+#define LED_B_Set()                   __GPIO_SET(LED_B_PORT, LED_B_PIN)
+#define LED_B_Reset()                 __GPIO_RST(LED_B_PORT, LED_B_PIN)
+#define LED_B_Toggle()                __GPIO_TOG(LED_B_PORT, LED_B_PIN)
+
+#define KEY_PIN                       11
+#define KEY_PORT                      NRF_P0
+#define KEY_Read()                    (__GPIO_READ(KEY_PORT, KEY_PIN) == SET)
+
+#define KEY_GPIOTEx                   NRF_GPIOTE
+#define KEY_GPIOTEx_LINE              0
+#define KEY_GPIOTEx_IRQn              GPIOTE_IRQn
+#define KEY_GPIOTEx_IRQn_PRIORITY     1
+#define KEY_GPIOTEx_IRQn_INT_LINE     NRF_GPIOTE_INT_IN0_MASK
+#define KEY_GPIOTEx_IRQn_POLARITY     NRF_GPIOTE_POLARITY_LOTOHI
+#define KEY_GPIOTEx_IRQn_MODE         GPIOTE_CONFIG_MODE_Event
+#define KEY_GPIOTEx_IRQn_OUTINIT      NRF_GPIOTE_INITIAL_VALUE_LOW
+
+
+/* -------- Timer */
+
+#define TIMERx                        NRF_TIMER0
+#define TIMERx_CHANNEL                NRF_TIMER_CC_CHANNEL0
+#define TIMERx_IRQn                   TIMER0_IRQn
+#define TIMERx_IRQn_PRIORITY          1
+
+#define TIMERx_MODE                   NRF_TIMER_MODE_TIMER
+#define TIMERx_PRESCALER              NRF_TIMER_FREQ_1MHz
+#define TIMERx_BIT_MODE               NRF_TIMER_BIT_WIDTH_32
+
+
+/* Exported functions ----------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/*************************************** END OF FILE ****************************************/
---
+++ firmwareNRF52/KSNRF_Peripheral_TIMER/Program/drivers/nrf5x_timer.h
@@ -0,0 +1,80 @@
+/**
+ *      __            ____
+ *     / /__ _  __   / __/                      __  
+ *    / //_/(_)/ /_ / /  ___   ____ ___  __ __ / /_ 
+ *   / ,<  / // __/_\ \ / _ \ / __// _ \/ // // __/ 
+ *  /_/|_|/_/ \__//___// .__//_/   \___/\_,_/ \__/  
+ *                    /_/   github.com/KitSprout    
+ * 
+ *  @file    nrf5x_timer.h
+ *  @author  KitSprout
+ *  @date    01-Dec-2017
+ *  @brief   
+ * 
+ */
+
+/* Define to prevent recursive inclusion ---------------------------------------------------*/
+#ifndef __NRF5x_TIMER_H
+#define __NRF5x_TIMER_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+/* Includes --------------------------------------------------------------------------------*/
+#include "drivers\nrf5x_system.h"
+
+/* Exported types --------------------------------------------------------------------------*/
+
+typedef struct {
+  NRF_TIMER_Type *Instance;
+
+  uint32_t Prescaler;
+  // NRF_TIMER_FREQ_16MHz   - Timer frequency 16 MHz.
+  // NRF_TIMER_FREQ_8MHz    - Timer frequency 8 MHz.
+  // NRF_TIMER_FREQ_4MHz    - Timer frequency 4 MHz.
+  // NRF_TIMER_FREQ_2MHz    - Timer frequency 2 MHz.
+  // NRF_TIMER_FREQ_1MHz    - Timer frequency 1 MHz.
+  // NRF_TIMER_FREQ_500kHz  - Timer frequency 500 kHz.
+  // NRF_TIMER_FREQ_250kHz  - Timer frequency 250 kHz.
+  // NRF_TIMER_FREQ_125kHz  - Timer frequency 125 kHz.
+  // NRF_TIMER_FREQ_62500Hz - Timer frequency 62500 Hz.
+  // NRF_TIMER_FREQ_31250Hz - Timer frequency 31250 Hz.
+  uint32_t Period;
+  // 32bit value
+  uint32_t Mode;
+  // NRF_TIMER_MODE_TIMER   - Timer in Normal mode.
+  // NRF_TIMER_MODE_COUNTER - Timer in Counter mode.
+  uint32_t BitMode;
+  // NRF_TIMER_BIT_WIDTH_8  -  8-bit timer behaviour.
+  // NRF_TIMER_BIT_WIDTH_16 - 16-bit timer behaviour.
+  // NRF_TIMER_BIT_WIDTH_24 - 24-bit timer behaviour.
+  // NRF_TIMER_BIT_WIDTH_32 - 32-bit timer behaviour.
+  pFunc EventCallback;
+  // TIMER EVENT CALLBACK
+} TIMER_TimeBaseInitTypeDef;
+
+/* Exported constants ----------------------------------------------------------------------*/
+/* Exported macro --------------------------------------------------------------------------*/
+
+#define TIMER_TASKS_START(__INS)            (__INS->TASKS_START = SET)          // Start Timer
+#define TIMER_TASKS_STOP(__INS)             (__INS->TASKS_STOP = SET)           // Stop Timer
+#define TIMER_TASKS_COUNT(__INS)            (__INS->TASKS_COUNT = SET)          // Increment Timer (Counter mode only)
+#define TIMER_TASKS_CLEAR(__INS)            (__INS->TASKS_CLEAR = SET)          // Clear time
+#define TIMER_TASKS_SHUTDOWN(__INS)         (__INS->TASKS_CLEAR = SET)          // Shut down timer
+#define TIMER_TASKS_CAPTURE(__INS, __CH)    (__INS->TASKS_CAPTURE[__CH] = SET)  // Capture Timer value to CC[x] register
+
+#define TIMER_EVENT_COPPARE(__INS, __CH)    __INS->EVENTS_COMPARE[__CH]         // Compare event on CC[x] match
+
+/* Exported functions ----------------------------------------------------------------------*/  
+void      TIMER_TimeBaseInit( TIMER_TimeBaseInitTypeDef *htimer );
+void      TIMER_Cmd( TIMER_TimeBaseInitTypeDef *htimer, uint8_t state );
+void      TIMER_CCInit( TIMER_TimeBaseInitTypeDef *htimer, uint32_t channel, uint32_t value );
+void      TIMER_InterruptCmd( TIMER_TimeBaseInitTypeDef *htimer, uint32_t channel, uint8_t state );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/*************************************** END OF FILE ****************************************/
